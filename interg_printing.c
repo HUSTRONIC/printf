4681dbb8c016856fa7c7dff4b2f8fe4a81a41e4b
SantosAndrewz <sentosandrewz@gmail.com>
2023-10-17 19:27:28 -0700

    update 2

---
+++ interg_printing.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * interg_printing - prints integer and decimals
+ * @i: integer value.
+ * @count: pointer to count.
+ *
+ * Return: count.
+ */
+int interg_printing(long int i, int *count)
+{
+	if (i < 0)
+	{
+		i = i * (-1);
+		_putchar('-');
+		*count +=  1;
+	}
+	if (i / 10)
+		interg_printing((i / 10), count);
+		*count += 1;
+		putchar((i % 10) + '0');
+	return (*count);
+}
