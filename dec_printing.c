55cd327ffa18ce1de52e3fd1a83efc90857026ba
SantosAndrewz <sentosandrewz@gmail.com>
2023-10-17 20:00:49 -0700

    3-update

---
+++ dec_printing.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * dec_printing - prints integer and decimals
+ * @i: integer value.
+ * @count: pointer to count.
+ *
+ * Return: count.
+ */
+int dec_printing(long int i, int *count)
+{
+        if (i < 0)
+        {
+                i = i * (-1);
+        
+		_putchar('-');
+		*count +=  1;
+        }
+        if (i / 10)
+	dec_printing((i / 10), count);
+	*count += 1;
+	putchar((i % 10) + '0');
+	return (*count);
+}
